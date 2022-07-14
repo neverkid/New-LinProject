962fb1f08bcb2496e17af6d942f574b9f5a7306c
Mr_Dreamt <stdio_c@163.com>
2022-07-14 10:18:06 +0800

    First commit

--- index.cpp
+++ index.cpp
@@ -4,7 +4,7 @@
 
 int main(){
     std::cout<<R"(Reset of Power)";
-    std::string str = R"(dir C:\*.dll)";
+    std::string str = R"(dir D:\Users\tons\AppData\Roaming\baidu\BaiduNetdisk\*.dll > DdllSetNote.txt)";
     system(str.c_str());
     return 0;
 }
