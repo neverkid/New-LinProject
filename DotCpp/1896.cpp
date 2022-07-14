f4b34c1f83160649f3fb1e8133b5acf13e9099ea
dream <l614928@163.com>
2022-07-14 17:13:09 +0800

    ll

--- DotCpp/1896.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-typedef unsigned long long int ulint; 
-int main(){
-    ulint a=(1<<31);
-    cout<<a<<endl;
-    return 0;
-}
--- main.cc
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-int main(){
-    std::cout<<"Hello World!,I am Dreamt!";
-    std::cout<<"\n";
-    return 0;
-}
