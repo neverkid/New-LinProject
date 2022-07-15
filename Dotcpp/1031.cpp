7862a774f1628e57f5a1a24064ecc54e51095362
Mr_Dreamt <stdio_c@163.com>
2022-07-15 11:10:12 +0800

    normal commit

---
+++ Dotcpp/1031.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+string sreverse(string str){
+    string res = str;
+    reverse(res.begin(),res.end());
+    return res;
+}
+
+int main(){
+    string str;
+    getline(cin,str);
+    cout<<sreverse(str)<<endl;
+    return 0;
+}
