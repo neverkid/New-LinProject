444f838113b1f7c25662c0791c8dd20b36e1a32e
myf <1586891929@qq.com>
2022-07-15 11:37:51 +0800

    myf_commit

--- Dotcpp/1025.cpp
+++ Dotcpp/1025.cpp
@@ -1,1 +1,20 @@
-#
+#include<stdio.h>
+int main(){
+	int a[10],n;
+	for(int i=0;i<9;i++){
+		scanf("%d",&a[i]);
+	}
+	scanf("%d",&n);
+	for(int i=8;i>=0;i--){
+		if(n<a[i]){
+			a[i+1]=a[i];
+		}else{
+			a[i+1]=n;
+			break;
+		}
+	}
+	for(int i=0;i<10;i++){
+		printf("%d\n",a[i]);
+	}
+	return 0;
+} 
